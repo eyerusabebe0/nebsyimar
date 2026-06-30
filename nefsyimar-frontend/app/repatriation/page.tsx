9328d4158acba8054dcc3ec44e641a802d697139
eyerusabebe0 <eyerusabebe0@gmail.com>
2026-06-30 10:28:55 +0300

    upadting tsconfig.tsbuildinfo

--- nefsyimar-frontend/app/repatriation/page.tsx
+++ nefsyimar-frontend/app/repatriation/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useEffect, useState } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { CheckCircle, Upload, AlertCircle, Plane, ShieldCheck, Truck, PhoneCall, Mail, ChevronRight, ChevronLeft } from 'lucide-react'
